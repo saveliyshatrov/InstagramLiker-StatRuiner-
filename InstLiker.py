e15fb03295d1145875ee87b82fb5b3ed8e22469b
time_guy <31863650+saveliyshatrov@users.noreply.github.com>
2020-08-19 17:00:16 +0300

    Update InstLiker.py

--- InstLiker.py
+++ InstLiker.py
@@ -4,23 +4,45 @@ import os
 import random
 
 class Liker:
-    def __init__(self, username: str, password: str, hashtags: list, numberOfLikes: int)-> None:
-        self.username = username
-        self.password = password
-        self.hashtags = hashtags
-        self.numberOfLikes = numberOfLikes
+    def __init__(self) -> None:
+        self.username = 'username'
+        self.password = 'password'
+        self.hashtags = []
+        self.numberOfLikes = 0
+        self.userToLike = 'username'
         self.urlForTags = 'https://www.instagram.com/explore/tags/'
+        self.urlForUsers = 'https://www.instagram.com/'
         self.XPath_username = '/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div/div[1]/div/label/input'
-        self.Xpath_password = '/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div/div[2]/div/label/input'
+        self.XPath_password = '/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div/div[2]/div/label/input'
         self.LoginButton = '/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div/div[3]/button'
-        self.XPath_post = '/html/body/div[1]/section/main/article/div[1]/div/div/div[1]/div[1]'
+        self.XPath_post = ''
+        self.Top = '/html/body/div[1]/section/main/article/div[1]/div/div/div[1]/div[1]'
+        self.Recent = '/html/body/div[1]/section/main/article/div[2]/div/div[1]/div[1]'
+
+        self.XPath_forUsersPage = '/html/body/div[1]/section/main/div/div[2]/article/div/div/div[1]/div[1]'
+        self.XPath_forUsersLike = '/html/body/div[4]/div[2]/div/article/div[3]/section[1]/span[1]/button'
+        self.XPath_forUsersNextPhoto_1 = '/html/body/div[4]/div[1]/div/div/a'
+        self.XPath_forUsersNextPhoto = '/html/body/div[4]/div[1]/div/div/a[2]'
         self.driver = webdriver.Firefox()
+
+        #__._sakura___._
 
     def checkPrevWork(self):
         if True:
             pass
         else:
             pass
+
+    def SetInfo(self, username:str = 'username', password:str = 'password', hashtags:list = [], numberOfLikes:int = 100, userToLike: str = 'userToLike', Top: bool = False) -> None:
+        self.username = username
+        self.password = password
+        self.hashtags = hashtags
+        self.numberOfLikes = numberOfLikes
+        self.userToLike = userToLike
+        if Top == False:
+            self.XPath_post = self.Recent
+        if Top == True:
+            self.XPath_post = self.Top
 
     def Sleep(self, time: int, showStatistics: bool, nowLikes: int = 0, allLikes: int = 0, hashtag: str = ''):
         time = random.randrange(int(time/3), time*2)
@@ -41,12 +63,16 @@ class Liker:
         self.Sleep(10, False)
         self.driver.find_element_by_xpath(self.XPath_username).send_keys(self.username)
         print("Username was entered")
-        self.driver.find_element_by_xpath(self.Xpath_password).send_keys(self.password)
+        self.driver.find_element_by_xpath(self.XPath_password).send_keys(self.password)
         print("Password was entered")
         self.driver.find_element_by_xpath(self.LoginButton).click()
         self.Sleep(5, False)
 
-    def Main(self):
+    def UserLiker(self):
+        pass
+    #
+
+    def HashtagLiker(self):
         for hashtag in self.hashtags:
             self.driver.get(self.urlForTags + hashtag + '/')
             self.Sleep(5, False)
@@ -82,13 +108,19 @@ class Liker:
                             else:
                                 self.driver.find_element_by_xpath('/html/body/div[4]/div[1]/div/div/a[2]').click()
 
-    def run(self):
+    def run(self, userLiking: bool = False):
         self.Login()
-        self.Main()
+        if userLiking == False:
+            self.HashtagLiker()
+        if userLiking == True:
+            self.UserLiker()
+
 
 String = "photographyart #photographyoftheday #photographyy #photographylove #photographyaddict #photographyskills #photographybook #photographyprops #photographydaily #photographyisart #photographyaccount #photographystudio #photographyday #photographynature #photographysoul #photographystudent #photographyworkshop #photographyindonesia #photographyblog #photographyig #photographybusiness #photography101"
 string = String.replace(" ", "")
 array = string.split("#")
 
 
-Liker('username', 'password', array, 100).run()
+Liker = Liker()
+Liker.SetInfo(username='username', password='password', hashtags=array, numberOfLikes=100)
+Liker.run()
